fb1a4cb7aff41b36094a8cac60c6d315c7e5d9bc
CodeDevMLH <145071728+CodeDevMLH@users.noreply.github.com>
2025-08-30 01:44:37 +0200

    ui change

--- spotizerr-ui/src/components/config/MediaServersTab.tsx
+++ spotizerr-ui/src/components/config/MediaServersTab.tsx
@@ -113,8 +113,8 @@ export function MediaServersTab() {
         </div>
         <div className="flex items-center justify-between">
           <label className="text-content-primary dark:text-content-primary-dark" htmlFor="intervalEnabled">Enable Interval Scan</label>
-          <p className="text-xs text-content-muted dark:text-content-muted-dark">Consider using the built-in functions in jellyfin/plex</p>
           <input id="intervalEnabled" type="checkbox" {...register("intervalEnabled")} className="h-6 w-6" />
+          <p className="text-xs text-content-muted dark:text-content-muted-dark">Consider using the built-in functions in jellyfin/plex</p>
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="intervalSeconds" className="text-content-primary dark:text-content-primary-dark">Interval (seconds)</label>
